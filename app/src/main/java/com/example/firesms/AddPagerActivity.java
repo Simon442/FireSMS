56d08c9e10b0cf789b8beaab91df460f85385a83
Simon <simon.ekart10@gmail.com>
2020-01-27 21:46:08 +0100

    - Dodan dialog za pozivnike - Dodana 2 permissiona

--- app/src/main/java/com/example/firesms/AddPagerActivity.java
+++ app/src/main/java/com/example/firesms/AddPagerActivity.java
@@ -1,8 +1,13 @@
 package com.example.firesms;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddPagerActivity extends AppCompatActivity {
 
@@ -11,5 +16,40 @@ public class AddPagerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_pager);
+
+        Button addPagerBtn = findViewById(R.id.addPagerBtn);
+        addPagerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(AddPagerActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.activity_add_pager_dialog,null);
+                final EditText mPagerName = mView.findViewById(R.id.textPagerName);
+                final EditText mPhoneNumber = mView.findViewById(R.id.textPhoneNumber);
+                final EditText mKeyword = mView.findViewById(R.id.textKeyword);
+                EditText mMessage = mView.findViewById(R.id.textMessage);
+                EditText mTitle = mView.findViewById(R.id.textFireSMS);
+                Button mAddPager = mView.findViewById(R.id.addPagerBtn);
+
+                mAddPager.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (!mPagerName.getText().toString().isEmpty() & mKeyword.getText().toString().isEmpty() & mPhoneNumber.getText().toString().isEmpty()){
+                            Toast.makeText(SettingsActivity.this, R.string.success_added_pager_msg, Toast.LENGTH_SHORT).show();
+                        }else {
+                            Toast.makeText(SettingsActivity.this, R.string.failed_add_pager_msg, Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+                mBuilder.setView(mView);
+                AlertDialog dialog = mBuilder.create();
+                dialog.show();
+
+
+            }
+        });
+
+
     }
 }
