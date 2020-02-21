78e339dc04b5c70faeed3f9ceb54eb592ad4f922
Simon <simon.ekart10@gmail.com>
2020-02-21 13:46:32 +0100

    Broadcast Receiver updated

---
+++ app/src/main/java/com/example/firesms/MySmsReceiver.java
@@ -0,0 +1,68 @@
+package com.example.firesms;
+
+import android.annotation.TargetApi;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MySmsReceiver extends BroadcastReceiver {
+
+    private static final String TAG =
+            MySmsReceiver.class.getSimpleName();
+
+    public static final String pdu_type = "pdus";
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Get the SMS message.
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] msgs;
+        String strMessage = "";
+        String format = bundle.getString("format");
+
+
+        // Retrieve the SMS message received.
+        Object[] pdus = (Object[]) bundle.get(pdu_type);
+        if (pdus != null) {
+
+
+            // Check the Android version.
+            boolean isVersionM =
+                    (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M);
+
+
+            // Fill the msgs array.
+            msgs = new SmsMessage[pdus.length];
+            for (int i = 0; i < msgs.length; i++) {
+
+
+                // Check Android version and use appropriate createFromPdu.
+                if (isVersionM) {
+
+
+                    // If Android version M or newer:
+                    msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i], format);
+                } else {
+
+
+                    // If Android version L or older:
+                    msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                }
+
+
+                // Build the message to show.
+                strMessage += "SMS from " + msgs[i].getOriginatingAddress();
+                strMessage += " :" + msgs[i].getMessageBody() + "\n";
+
+
+                // Log and display the SMS message.
+                Log.d(TAG, "onReceive: " + strMessage);
+                Toast.makeText(context, strMessage, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
