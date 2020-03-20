a1f2bc980febf8c5986ed347232597d0e10fd8f9
Simon <simon.ekart10@gmail.com>
2020-03-20 15:01:34 +0100

    - reverted changes to buttons instead of nav bar

--- app/src/main/java/com/example/firesms/MainActivity.java
+++ app/src/main/java/com/example/firesms/MainActivity.java
@@ -1,19 +1,15 @@
 package com.example.firesms;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     private SharedPreferences pref;
@@ -30,37 +26,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //TODO Poglej kaj je tukaj narobe da crasha
-
-
-
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_bar);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        Button addPagerBtn = findViewById(R.id.addPagerBtn);
+        addPagerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-
-                switch (menuItem.getItemId()) {
-                    case R.id.menuDrawerBtn:
-                        Intent intent1 = new Intent(MainActivity.this, MainActivity.class);
-                        startActivity(intent1);
-                        break;
-
-                    case R.id.addPagerDrawerBtn:
-                        Intent intent2 = new Intent(MainActivity.this, AddPagerActivity.class);
-                        startActivity(intent2);
-                        break;
-
-                    case R.id.settingsDrawerBtn:
-                        Intent intent3 = new Intent(MainActivity.this, SettingsActivity.class);
-                        startActivity(intent3);
-                        break;
-                }
-                return false;
+            public void onClick(View v) {
+                openActivity(AddPagerActivity.class);
             }
         });
 
-        Button devOptionsBtn = findViewById(R.id.devOptionsBtn);
+        Button settingsBtn = findViewById(R.id.settingsBtn);
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity(SettingsActivity.class);
+            }
+        });
+
+        Button devOptionsBtn = findViewById(R.id.settingsBtn);
         devOptionsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
