ce55c89570299cf875757ec16542f0ef43701c80
Simon <simon.ekart10@gmail.com>
2020-03-22 17:18:35 +0100

    - added permissions

--- app/src/main/java/com/example/firesms/MainActivity.java
+++ app/src/main/java/com/example/firesms/MainActivity.java
@@ -97,4 +97,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void checkPermissions(View view) {
+    }
 }
--- app/src/main/java/com/example/firesms/SplashScreenActivity.java
+++ app/src/main/java/com/example/firesms/SplashScreenActivity.java
@@ -21,4 +21,5 @@ public class SplashScreenActivity extends AppCompatActivity {
             }
         });
     }
+
 }
