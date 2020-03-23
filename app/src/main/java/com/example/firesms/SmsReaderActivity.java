70444db4a52f8cafd153e89b491bd55c078b477e
Simon <simon.ekart10@gmail.com>
2020-03-23 15:01:50 +0100

    - added SMS reader

---
+++ app/src/main/java/com/example/firesms/SmsReaderActivity.java
@@ -0,0 +1,80 @@
+package com.example.firesms;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class SmsReaderActivity extends AppCompatActivity {
+
+    ListView listView;
+    private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
+    ArrayList<String> smsList;
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.pagerList);
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_SMS);
+
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            showContacts();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_SMS},
+                    PERMISSIONS_REQUEST_READ_CONTACTS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_READ_CONTACTS) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                showContacts();
+            }else {
+                Toast.makeText(this, "FireSMS mogoče ne bo deloval pravilno", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void showContacts() {
+        Uri inboxUri = Uri.parse("content://sms/inbox");
+        smsList = new ArrayList<>();
+        ContentResolver contentResolver = getContentResolver();
+
+        Cursor cursor = contentResolver.query(inboxUri, null, null, null, null);
+        while (cursor.moveToNext()) {
+            String date = cursor.getString(cursor.getColumnIndexOrThrow("date")).toString();
+            String number = cursor.getString(cursor.getColumnIndexOrThrow("address")).toString();
+            String body = cursor.getString(cursor.getColumnIndexOrThrow("body")).toString();
+
+            smsList.add("Number: "+number+ "\n" + "Body: "+body);
+            Log.d(TAG, "showContacts: number: "+number+" body:" +body);
+        }
+        cursor.close();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                smsList);
+
+        listView.setAdapter(adapter);
+    }
+}
