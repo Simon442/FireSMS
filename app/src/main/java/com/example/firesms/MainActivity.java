698d28eed8aa6b8fb4418cfdca76b4bb89361f67
Simon <simon.ekart10@gmail.com>
2020-03-31 13:54:36 +0200

    - new permissions

--- app/src/main/java/com/example/firesms/MainActivity.java
+++ app/src/main/java/com/example/firesms/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.firesms;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -10,6 +11,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.Toast;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private SharedPreferences pref;
@@ -94,6 +101,33 @@ public class MainActivity extends AppCompatActivity {
 
             openActivity(SplashScreenActivity.class);
         }
+
+    }
+
+    public void checkPermissions(View view) {
+
+        PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+            }
+
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions) {
+
+            }
+        };
+
+        TedPermission.with(this)
+                .setPermissionListener(permissionListener)
+                .setDeniedMessage("FireSMS morda ne bo deloval pravilno\n\nPritisni še enkrat na Vklopi ali pojdi v nastavitve")
+                .setPermissions(Manifest.permission.READ_CONTACTS,
+                        Manifest.permission.ACCESS_NOTIFICATION_POLICY,
+                        Manifest.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS,
+                        Manifest.permission.READ_SMS,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_PHONE_NUMBERS,
+                        Manifest.permission.CALL_PHONE)
+                .check();
 
     }
 }
--- app/src/main/java/com/example/firesms/SplashScreenActivity.java
+++ app/src/main/java/com/example/firesms/SplashScreenActivity.java
@@ -2,9 +2,16 @@ package com.example.firesms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.List;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -21,5 +28,5 @@ public class SplashScreenActivity extends AppCompatActivity {
             }
         });
     }
-
 }
+
