b6ec7d358cbe4e7639e616ae8883821cb8cced64
Simon <simon.ekart10@gmail.com>
2020-04-15 19:31:08 +0200

    - sq lite

---
+++ app/src/main/java/com/example/firesms/AddNewPagerActivity.java
@@ -0,0 +1,6 @@
+package com.example.firesms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddNewPagerActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/firesms/SQLiteDatabase.java
@@ -0,0 +1,35 @@
+package com.example.firesms;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SQLiteDatabase extends SQLiteOpenHelper {
+    @Override
+    public void onCreate(android.database.sqlite.SQLiteDatabase db) {
+
+        // If you change the database stuff, you must increment the database version.
+        public static final int DATABASE_VERSION = 1;
+        public static final String DATABASE_NAME = "FIRESMS.db";
+
+    public Context context;
+        SQLiteDatabase(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL( "CREATE TABLE " + DATABASE_NAME + "(" +
+                    "NAME VARCHAR(25) PRIMARY KEY," +
+                    "PHONE_NUMBER VARCHAR(25) NOT NULL," +
+                    "KEYWORD VARCHAR(15), NOT NULL);");
+        }
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            // This database is only a cache for online data, so its upgrade policy is
+            // to simply to discard the data and start over
+            db.execSQL("DROP TABLE IF EXISTS " + DATABASE_NAME);
+            onCreate(db);
+        }
+        public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            onUpgrade(db, oldVersion, newVersion);
+        }
+
+    }
+}
