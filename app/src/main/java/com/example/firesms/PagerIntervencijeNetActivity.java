acedbfbf7a9f8a0a64c3b54d32b3e871c6318bf4
Simon <simon.ekart10@gmail.com>
2020-04-24 13:25:18 +0200

    - intervencije.net dialog buttons call assigned (test) numbers

--- app/src/main/java/com/example/firesms/PagerIntervencijeNetActivity.java
+++ app/src/main/java/com/example/firesms/PagerIntervencijeNetActivity.java
@@ -1,8 +1,14 @@
 package com.example.firesms;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class PagerIntervencijeNetActivity extends AppCompatActivity {
 
@@ -10,5 +16,47 @@ public class PagerIntervencijeNetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pager_intervencije_net);
+    }
+
+
+    public void Not_coming_call(View view) {
+        String number = "111111";
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:" + number));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+
+        }
+
+        startActivity(intent);
+    }
+
+    public void Coming_call(View view) {
+        String number = "222222";
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:" + number));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+
+        }
+
+        startActivity(intent);
+
+    }
+
+    public void Coming_later_call(View view) {
+        String number = "333333";
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:" + number));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+
+        }
+
+        startActivity(intent);
+
     }
 }
