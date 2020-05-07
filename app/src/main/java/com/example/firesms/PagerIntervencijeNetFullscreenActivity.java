3c9e22ef384813dca42fa032d42275aba0a04cae
Simon <simon.ekart10@gmail.com>
2020-05-07 22:41:01 +0200

    - New alert UI done for Intervencije.net
    TODO new UI for normal pager alert

--- app/src/main/java/com/example/firesms/PagerIntervencijeNetFullscreenActivity.java
+++ app/src/main/java/com/example/firesms/PagerIntervencijeNetFullscreenActivity.java
@@ -1,8 +1,17 @@
 package com.example.firesms;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Vibrator;
+import android.util.Log;
+import android.view.View;
 
 public class PagerIntervencijeNetFullscreenActivity extends AppCompatActivity {
 
@@ -10,5 +19,75 @@ public class PagerIntervencijeNetFullscreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pager_intervencije_net_fullscreen);
+    }
+
+    public void Not_coming_call(View view) {
+
+        String number = "111111";
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:" + number));
+        Vibrator v = (Vibrator) getApplicationContext().getSystemService(getApplicationContext().VIBRATOR_SERVICE);
+        v.vibrate(100);
+        ((new Handler())).postDelayed(new Runnable(){
+            public void run(){
+
+                Log.i("I","TEST I AM IN RUN");
+            }
+        }, 100);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+
+        startActivity(intent);
+        finish();
+    }
+
+    public void Coming_call(View view) {
+        String number = "222222";
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:" + number));
+        Vibrator v = (Vibrator) getApplicationContext().getSystemService(getApplicationContext().VIBRATOR_SERVICE);
+        v.vibrate(100);
+        ((new Handler())).postDelayed(new Runnable(){
+            public void run(){
+
+                Log.i("I","TEST I AM IN RUN");
+            }
+        }, 100);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+
+        startActivity(intent);
+        finish();
+
+    }
+
+    public void Coming_later_call(View view) {
+        String number = "333333";
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:" + number));
+        Vibrator v = (Vibrator) getApplicationContext().getSystemService(getApplicationContext().VIBRATOR_SERVICE);
+        v.vibrate(100);
+        ((new Handler())).postDelayed(new Runnable(){
+            public void run(){
+
+                Log.i("I","TEST I AM IN RUN");
+            }
+        }, 100);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+
+        startActivity(intent);
+        finish();
+    }
+
+    public void Close_dialog(View view) {
+
+        finish();
     }
 }
